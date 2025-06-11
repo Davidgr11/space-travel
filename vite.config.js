f7a528dd8f6a17c0c4b6026eae0d0539c048c0d9
davidgr11 <davidygonzalez@hotmail.com>
2025-06-11 12:41:29 -0600

    fix: redirects

--- vite.config.js
+++ vite.config.js
@@ -9,19 +9,8 @@ export default defineConfig({
 			input: {
 				main: 'index.html',
 				destination: 'destination.html',
-				'destination-moon': 'destination-moon.html',
-				'destination-mars': 'destination-mars.html',
-				'destination-europa': 'destination-europa.html',
-				'destination-titan': 'destination-titan.html',
 				crew: 'crew.html',
-				'crew-commander': 'crew-commander.html',
-				'crew-specialist': 'crew-specialist.html',
-				'crew-pilot': 'crew-pilot.html',
-				'crew-engineer': 'crew-engineer.html',
-				technology: 'technology.html',
-				'technology-vehicle': 'technology-vehicle.html',
-				'technology-spaceport': 'technology-spaceport.html',
-				'technology-capsule': 'technology-capsule.html'
+				technology: 'technology.html'
 			}
 		}
 	}
