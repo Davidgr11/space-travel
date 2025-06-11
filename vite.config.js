1ca4b934d0901dffceae7c77ea82eaee2cbe1f63
davidgr11 <davidygonzalez@hotmail.com>
2025-06-11 12:37:22 -0600

    fix: redirects

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import {defineConfig} from "vite"
 export default defineConfig({
 	plugins: [
 		
-	]
+	],
+	build: {
+		rollupOptions: {
+			input: {
+				main: 'index.html',
+				destination: 'destination.html',
+				crew: 'crew.html',
+				technology: 'technology.html'
+			}
+		}
+	}
 })
